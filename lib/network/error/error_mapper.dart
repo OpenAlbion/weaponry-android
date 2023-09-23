1d35efc4474f0fe15257ca959a624e27e04151df
thurain <thurainbowin.dev@gmail.com>
2023-09-23 09:20:17 +0630

    wip

--- lib/network/error/error_mapper.dart
+++ lib/network/error/error_mapper.dart
@@ -4,34 +4,23 @@ import 'package:dio/dio.dart';
 import 'package:openalbion_weaponry/data/vos/app_error.dart';
 import 'package:openalbion_weaponry/network/api_constants.dart';
 
-
-
 class ErrorMapper {
   static mapDioToAppError(DioError dioError) {
     switch (dioError.type) {
       case DioErrorType.connectionTimeout:
-        {
-          return AppError(
-              code: ApiConstants.STATUS_CODE_CONNECT_TIMEOUT,
-              message: "Connect Timeout");
-        }
+        return AppError(code: ApiConstants.STATUS_CODE_CONNECT_TIMEOUT, message: "Connect Timeout");
 
       case DioErrorType.receiveTimeout:
-        {
-          return AppError(
-              code: ApiConstants.STATUS_CODE_RECEIVE_TIMEOUT,
-              message: "Response Timeout");
-        }
+        return AppError(code: ApiConstants.STATUS_CODE_RECEIVE_TIMEOUT, message: "Response Timeout");
 
       case DioErrorType.badResponse:
         return AppError(
             code: dioError.response!.statusCode.toString(),
-            message:
-                takeMessageFromDioResponseJson(dioError.response.toString()));
+            message: takeMessageFromDioResponseJson(dioError.response.toString()));
 
       default:
         {
-          var message = dioError.error ?? "unknown error ";
+          var message = dioError.error ?? "unknown dio error";
           return AppError(code: "-", message: message.toString());
         }
     }
